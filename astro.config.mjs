6dd523870365c8b34ab7824525e7c5cc42382c5a
redmenge <redondomendoza.german@gmail.com>
2025-05-12 20:41:48 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://',
+    site: 'https://redmenge.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
