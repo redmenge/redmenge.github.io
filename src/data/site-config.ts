1ba7a79aff292f2a2700d5016ef3a38bd42a65e6
redmenge <redondomendoza.german@gmail.com>
2025-05-12 22:55:55 -0400

    Update site info

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -99,7 +99,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Hello and welcome to my room in the internet for you to visit',
-        text: "I'm **Germán Redondo**, a Network Engineer taking the cybersecurity pathway, dedicated to absorbing knowledge of today's technology in a daily basis. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. Feel free to explore some of my projects and on <a href='https://github.com/redmenge'>GitHub</a> or follow me on <a href='www.linkedin.com/in/german-redondo'>LinkedIn</a>.",
+        text: "I'm **Germán Redondo**, a Network Engineer taking the cybersecurity pathway, dedicated to absorbing knowledge of today's technology in a daily basis. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. Feel free to explore some of my projects and work on <a href='https://github.com/redmenge'>GitHub</a> or follow me on <a href='www.linkedin.com/in/german-redondo'>LinkedIn</a>.",
         image: {
             src: '/about.jpg',
             alt: 'A person sitting at a desk in front of a computer'
